bdf61070bd95da0ca23771e74b84beb9221aef63
A.Diana <a.diana.dev@gmail.com>
2024-04-23 22:35:46 +0200

    feat: add swagger gen for development

--- src/PokedexApi/Program.cs
+++ src/PokedexApi/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Hello World!")
+    .WithOpenApi();
 
 app.Run();
