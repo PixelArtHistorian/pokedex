a07f3f5b4f8616b6362b282e0b50bab0ec106928
A.Diana <a.diana.dev@gmail.com>
2024-04-26 16:37:37 +0200

    fix: add habitat edge case to mapper

--- src/PokedexApiTest/PokemonMapperTest.cs
+++ src/PokedexApiTest/PokemonMapperTest.cs
@@ -62,7 +62,7 @@ namespace PokedexApiTest
         [Theory]
         [InlineData("mewtwo", "Dieses Pokémon ist das Resultat eines jahrelangen\nund skrupellosen Experimentes.", "rare", true, "de")]
         [InlineData("mewtwo", "한 과학자가 몇 년에 걸쳐\n무서운 유전자의 연구를\n계속한 결과 탄생했다.", "rare", true, "ko")]
-        public void Mapper_Maps_PokemonResponse_WithPlaceholder_WhenEnglishNotAvailable(
+        public void Mapper_Maps_PokemonResponse_WithPlaceholderDescription_WhenEnglishNotAvailable(
             string pokemonName,
             string description,
             string habitatName,
@@ -74,11 +74,41 @@ namespace PokedexApiTest
             //Act
             var result = Sut.Map(response);
             //Assert
-            result.Name.Should().Be(pokemonName);
             result.Description.Should().Be("No english description available");
-            result.Habitat.Should().Be(habitatName);
-            result.IsLegendary.Should().Be(isLegendary);
+        }
 
+        [Fact]
+        public void Mapper_Maps_PokemonResponse_WithPlaceholderHabitat_WhenHabitatNotAvailable()
+        {
+            string pokemonName = "mewtwo";
+            string description = "It was created by a scientist after years of horrific gene splicing and DNA engineering experiments.";
+            string habitatName = null!;
+            bool isLegendary = true;
+            string language = "en";
+            //Arrange
+            PokemonResponse response = PokemonResponseFactory.CreatePokemonResponse(pokemonName, description, habitatName, isLegendary, language);
+            response.habitat = null!;
+            //Act
+            var result = Sut.Map(response);
+            //Assert
+            result.Habitat.Should().Be("This pokemon has no known habitat");
+        }
+
+        [Fact]
+        public void Mapper_Maps_PokemonResponse_WithPlaceholderHabitat_WhenHabitatNameNotAvailable()
+        {
+            string pokemonName = "mewtwo";
+            string description = "It was created by a scientist after years of horrific gene splicing and DNA engineering experiments.";
+            string habitatName = null!;
+            bool isLegendary = true;
+            string language = "en";
+            //Arrange
+            PokemonResponse response = PokemonResponseFactory.CreatePokemonResponse(pokemonName, description, habitatName, isLegendary, language);
+
+            //Act
+            var result = Sut.Map(response);
+            //Assert
+            result.Habitat.Should().Be("This pokemon has no known habitat");
         }
     }
 }
